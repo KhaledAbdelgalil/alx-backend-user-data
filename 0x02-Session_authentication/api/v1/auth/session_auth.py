3d4c1faebea4c3576bfbbc9f930f7106225659b0
root <root@DESKTOP-2I6K092>
2024-09-04 21:46:50 +0100

    task 1-2 in session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Definition of class SessionAuth
+"""
+import base64
+from uuid import uuid4
+from typing import TypeVar
+
+from .auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Implement Session Authorization protocol methods
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
